a30525e833c4ab3c10553633f5da55db4daf224a
Matt Foster <fostmatt@Matts-MacBook-Pro.local>
2021-12-29 00:58:16 -0700

    inquirer added, index file added

---
+++ index.js
@@ -0,0 +1,11 @@
+
+
+// link to created html page
+const generateHTML = require('./src/teamprofile');
+
+
+const Manager = require('./lib/Manager');
+const Intern = require('./lib/Intern');
+const Engineer = require('./lib/Engineer');
+const fs = require('fs');
+const inquirer = require('inquirer');
